b689f460b03d4636f02bf2dd2c31fc86d5ee5b87
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-17 21:21:40 +0530

    Created Security Questions DTO class with two data members

--- src/main/java/com/example/revpassword_manager/DTOs/LoginRequest.java
+++ src/main/java/com/example/revpassword_manager/DTOs/LoginRequest.java
@@ -1,5 +1,8 @@
 package com.example.revpassword_manager.DTOs;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
     private String username;
     private String password;
---
+++ src/main/java/com/example/revpassword_manager/DTOs/SecurityQuestionDTO.java
@@ -0,0 +1,9 @@
+package com.example.revpassword_manager.DTOs;
+
+import lombok.Data;
+
+@Data
+public class SecurityQuestionDTO {
+    private String question;
+    private String answer;
+}
