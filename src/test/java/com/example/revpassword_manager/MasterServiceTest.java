a95179a2d1488216f54295aaa1480229bb1c8fcf
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-17 20:57:28 +0530

    Created one test class to check the user is regering or not

---
+++ src/test/java/com/example/revpassword_manager/MasterServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.revpassword_manager;
+
+import com.example.revpassword_manager.Reposiotory.UserRepository;
+import com.example.revpassword_manager.Security.AuthService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class MasterServiceTest {
+
+
+
+        @Mock
+        private UserRepository userRepo;
+
+        @InjectMocks
+        private AuthService authService;
+
+        @Test
+        void testRegister() {
+
+            RegisterRequest req = new RegisterRequest();
+            req.setUsername("test");
+
+            String result = authService.register(req);
+
+            assertEquals("User Registered Successfully", result);
+        }
+    }
+
+
