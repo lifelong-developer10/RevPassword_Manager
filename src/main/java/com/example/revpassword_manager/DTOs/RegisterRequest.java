e0b4436966c0d16954ad970db2024ad3dbaf1cf3
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-17 21:25:16 +0530

    Created All Dto Classes

---
+++ src/main/java/com/example/revpassword_manager/DTOs/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.example.revpassword_manager.DTOs;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RegisterRequest {
+
+    private String username;
+    private String email;
+    private String phone;
+    private String password;
+
+
+    private List<SecurityQuestionDTO> securityQuestions;
+
+}
