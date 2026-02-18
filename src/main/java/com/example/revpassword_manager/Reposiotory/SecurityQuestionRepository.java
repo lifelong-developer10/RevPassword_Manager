e2853a8f5f40b83560850bce3169769e715af0fa
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-18 16:38:35 +0530

    Added the Security Questions Logic

--- src/main/java/com/example/revpassword_manager/Reposiotory/SecurityQuestionRepository.java
+++ src/main/java/com/example/revpassword_manager/Reposiotory/SecurityQuestionRepository.java
@@ -4,8 +4,10 @@ import com.example.revpassword_manager.Models.MasterUser;
 import com.example.revpassword_manager.Models.SecurityQuestions;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SecurityQuestionRepository extends JpaRepository<SecurityQuestions,Long> {
+    List<SecurityQuestions> findByUser(MasterUser user);
 
 }
