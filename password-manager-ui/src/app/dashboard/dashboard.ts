652f69b7f0103292b1d1e770d30f0320c9d4ace6
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-22 14:46:17 +0530

    updating frontend code

--- password-manager-ui/src/app/dashboard/dashboard.ts
+++ password-manager-ui/src/app/dashboard/dashboard.ts
@@ -1,11 +1,82 @@
 import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { VaultService } from '../services/vault.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  templateUrl: './dashboard.component.html'
 })
-export class Dashboard {
+export class DashboardComponent implements OnInit {
 
+  vault: any[] = [];
+  search = '';
+
+  form: any = {
+    website: '',
+    username: '',
+    password: ''
+  };
+
+  constructor(private vaultService: VaultService,
+              private auth: AuthService) {}
+
+  ngOnInit() {
+    this.loadVault();
+  }
+
+  loadVault() {
+    this.vaultService.getAll()
+      .subscribe((res: any) => {
+        this.vault = res;
+      });
+  }
+
+  addAccount() {
+
+    this.vaultService.add(this.form)
+      .subscribe(() => {
+
+        Swal.fire('Success','Account added','success');
+
+        this.loadVault();
+
+      });
+  }
+
+  delete(id: number) {
+
+    this.vaultService.delete(id)
+      .subscribe(() => {
+
+        Swal.fire('Deleted','Account removed','success');
+
+        this.loadVault();
+
+      });
+  }
+
+  viewPassword(v: any) {
+
+    Swal.fire({
+      title: 'Enter Master Password',
+      input: 'password',
+      showCancelButton: true
+    }).then(result => {
+
+      if (result.value) {
+
+        Swal.fire('Password', v.password, 'info');
+
+      }
+
+    });
+
+  }
+
+  logout() {
+    this.auth.logout();
+    location.href = '/login';
+  }
 }
