9e74d865206f2c27a1a5be8d320b60dc36a8cfbb
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-22 14:51:26 +0530

    updating frontend code

--- password-manager-ui/src/app/core/services/vault.ts
+++ password-manager-ui/src/app/core/services/vault.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Vault {
-  
+export class VaultService {
+
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  // Get all passwords
+  getAll() {
+    return this.http.get(`${this.baseUrl}/passwords`);
+  }
+
+  // Add password
+  add(data: any) {
+    return this.http.post(`${this.baseUrl}/passwords`, data);
+  }
+
+  // Update password
+  update(id: number, data: any) {
+    return this.http.put(`${this.baseUrl}/passwords/${id}`, data);
+  }
+
+  // Delete password
+  delete(id: number) {
+    return this.http.delete(`${this.baseUrl}/passwords/${id}`);
+  }
+
 }
--- password-manager-ui/src/app/vault/vault-list/vault-list.ts
+++ password-manager-ui/src/app/vault/vault-list/vault-list.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VaultService } from '../../core/services/vault.service';
 
 @Component({
   selector: 'app-vault-list',
-  imports: [],
-  templateUrl: './vault-list.html',
-  styleUrl: './vault-list.css',
+  templateUrl: './vault-list.html'
 })
-export class VaultList {
+export class VaultListComponent implements OnInit {
+
+  vault: any[] = [];
+
+  constructor(private vaultService: VaultService) {}
+
+  ngOnInit() {
+    this.loadVault();
+  }
+
+  loadVault() {
+    this.vaultService.getAll()
+      .subscribe((res: any) => {
+        this.vault = res;
+      });
+  }
 
 }
