3902b2322728c619722cf47bde188e41003b414f
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-22 15:38:13 +0530

    updating profile frontend code

--- password-manager-ui/src/app/profile/profile.ts
+++ password-manager-ui/src/app/profile/profile.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
-  templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  templateUrl: './profile.html'
 })
-export class Profile {
+export class ProfileComponent {
+
+  user = {
+    name: '',
+    email: '',
+    phone: ''
+  };
 
 }
