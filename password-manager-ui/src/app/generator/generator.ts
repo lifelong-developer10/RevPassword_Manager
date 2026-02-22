96a3b0ff9948fe70edeea748614779e97cc9caa5
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-22 15:37:17 +0530

    updating frontend code

--- password-manager-ui/src/app/generator/generator.ts
+++ password-manager-ui/src/app/generator/generator.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-generator',
-  imports: [],
-  templateUrl: './generator.html',
-  styleUrl: './generator.css',
+  templateUrl: './generator.html'
 })
-export class Generator {
+export class GeneratorComponent {
+
+  password = '';
+
+  generate() {
+
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%';
+
+    let pass = '';
+
+    for (let i = 0; i < 12; i++) {
+      pass += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    this.password = pass;
+  }
 
 }
