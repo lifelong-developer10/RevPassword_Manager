a268414fc1ba58e6bf8f91b1cb428a31c23be4a7
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-22 17:21:23 +0530

    updating frontend forgot password code

--- password-manager-ui/src/app/core/services/auth.ts
+++ password-manager-ui/src/app/core/services/auth.ts
@@ -59,4 +59,40 @@ getSecurityQuestions() {
 updateSecurityQuestions(data: any) {
   return this.http.put(`${this.baseUrl}/api/security-questions`, data);
 }
+requestRecovery(email: string) {
+  return this.http.post(`${this.baseUrl}/api/auth/forgot-password`, { email });
+}
+
+sendOtp(email: string) {
+   this.method = 'otp';
+  return this.http.post(`${this.baseUrl}/api/auth/send-otp`, { email });
+}
+
+verifyOtp(data: any) {
+  return this.http.post(`${this.baseUrl}/api/auth/verify-otp`, data);
+}
+
+getRecoveryQuestions(email: string) {
+  this.method = 'questions';
+  return this.http.get(`${this.baseUrl}/api/security-questions/recovery?email=${email}`);
+}
+
+verifySecurityAnswers(data: any) {
+  return this.http.post(`${this.baseUrl}/api/security-questions/verify`, data);
+}
+
+resetPassword(data: any) {
+  return this.http.post(`${this.baseUrl}/api/auth/reset-password`, data);
+}
+enable2FA() {
+  return this.http.post(`${this.baseUrl}/api/auth/2fa/enable`, {});
+}
+
+verify2FA(code: string) {
+  return this.http.post(`${this.baseUrl}/api/auth/2fa/verify`, { code });
+}
+
+disable2FA() {
+  return this.http.post(`${this.baseUrl}/api/auth/2fa/disable`, {});
+}
 }
