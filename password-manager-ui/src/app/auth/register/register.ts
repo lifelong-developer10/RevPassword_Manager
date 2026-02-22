4892f4e8154002d2207885a84719d62ec2201795
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-22 13:02:10 +0530

    adding frontend code

--- password-manager-ui/src/app/auth/register/register.ts
+++ password-manager-ui/src/app/auth/register/register.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
+  templateUrl: './register.component.html'
 })
-export class Register {
+export class RegisterComponent {
 
+  showPassword = false;
+
+  form = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@gmail\\.com$')
+    ]],
+    phone: ['', [
+      Validators.required,
+      Validators.pattern('^[0-9]{10}$')
+    ]],
+    password: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder,
+              private auth: AuthService) {}
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  register() {
+    if (this.form.invalid) return;
+
+    this.auth.register(this.form.value)
+      .subscribe({
+        next: () => {
+          Swal.fire('Success', 'Registration successful', 'success');
+        },
+        error: () => {
+          Swal.fire('Error', 'Registration failed', 'error');
+        }
+      });
+  }
 }
