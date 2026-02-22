28dbf94be2608c53ddaf060a6a81559f46ee658e
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-02-22 17:17:28 +0530

    updating frontend fprgot password  code

--- password-manager-ui/src/app/auth/forgot-password/forgot-password.ts
+++ password-manager-ui/src/app/auth/forgot-password/forgot-password.ts
@@ -1,11 +1,106 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
-  templateUrl: './forgot-password.html',
-  styleUrl: './forgot-password.css',
+  templateUrl: './forgot-password.html'
 })
-export class ForgotPassword {
+export class ForgotPasswordComponent {
+
+  step = 1;
+
+  email = '';
+  method = '';
+
+  otp = '';
+  newPassword = '';
+
+  questions: any[] = [];
+
+  constructor(private authService: AuthService) {}
+
+  // ---------------- STEP 1 ----------------
+
+  sendMethods() {
+
+    this.authService.requestRecovery(this.email)
+      .subscribe(() => {
+
+        this.step = 2;
+
+      });
+
+  }
+
+  // ---------------- OTP ----------------
+
+  sendOtp() {
+
+    this.authService.sendOtp(this.email)
+      .subscribe(() => {
+
+        Swal.fire('OTP Sent','Check your email','success');
+        this.step = 3;
+
+      });
+
+  }
+
+  verifyOtp() {
+
+    this.authService.verifyOtp({
+      email: this.email,
+      otp: this.otp
+    }).subscribe(() => {
+
+      this.step = 4;
+
+    });
+
+  }
+
+  // ---------------- SECURITY QUESTIONS ----------------
+
+  loadQuestions() {
+
+    this.authService.getRecoveryQuestions(this.email)
+      .subscribe((res: any) => {
+
+        this.questions = res;
+        this.step = 3;
+
+      });
+
+  }
+
+  verifyQuestions() {
+
+    this.authService.verifySecurityAnswers({
+      email: this.email,
+      questions: this.questions
+    }).subscribe(() => {
+
+      this.step = 4;
+
+    });
+
+  }
+
+  // ---------------- RESET PASSWORD ----------------
+
+  resetPassword() {
+
+    this.authService.resetPassword({
+      email: this.email,
+      newPassword: this.newPassword
+    }).subscribe(() => {
+
+      Swal.fire('Success','Password reset','success');
+      this.step = 1;
+
+    });
+
+  }
 
 }
